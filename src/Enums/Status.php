e4b6e316e3193575930383506a84ed4f99c09da0
Marco De Felice <io@marcodefelice.it>
2025-06-30 16:56:51 +0200

    fix status

--- src/Enums/Status.php
+++ src/Enums/Status.php
@@ -25,4 +25,14 @@ enum Status: string
             self::PAUSED => 'Paused',
         };
     }
+
+    /**
+     * Get all status values.
+     *
+     * @return array
+     */
+    public static function getValues(): array
+    {
+        return array_map(fn($status) => $status->value, self::cases());
+    }
 }
